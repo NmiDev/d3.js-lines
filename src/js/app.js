599fa02860276e33a6c2f69b9ffb17bfa7acc549
NmiDev <nicolas.missiaen@gmail.com>
2020-02-21 11:54:23 +0100

    Handle event and activity selection

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,35 @@
 const app = {
     // Properties
+    buttons: document.getElementsByTagName('button'),
+    form: document.querySelector('form'),
+    formTitle: document.querySelector('form span'),
+    input: document.querySelector('form input'),
+    monitor: document.getElementById('monitor'),
+    activity: 'activity-1',
+
 
     // App init
     init: function() {
-        console.log("app loaded");
-    }
+        for (const button of app.buttons) {
+            button.addEventListener('click', app.handleSelection);
+        }
+    },
 
     // Methods
+    handleSelection: function(evt) {
+        // Catch actiity from selection
+        app.activity = evt.target.getAttribute('data-activity');
+        // Toggle active class
+        for (const button of app.buttons) {
+            button.classList.remove('active');
+        }
+
+        evt.target.classList.add('active');
+        // Set ID for input field
+        app.input.id = app.activity;
+        // Set form title
+        app.formTitle.textContent = evt.target.textContent;
+    }
 };
 
 document.addEventListener('DOMContentLoaded', app.init);
